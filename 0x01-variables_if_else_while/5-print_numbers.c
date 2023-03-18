71dea963b135a81600ce2bd67f27fe36f448f76c
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-18 16:14:45 -0700

    Print all numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print numbers of base 10 starting from 0'
+ *Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
