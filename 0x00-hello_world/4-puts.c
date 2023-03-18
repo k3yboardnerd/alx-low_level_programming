cdab8a23246a43671ea11252ded885c285f478f1
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-17 19:52:56 -0700

    Fixed 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *Description: 'Prints "Programming is like building a multilingual puzzle"'
+ *Return: always 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
