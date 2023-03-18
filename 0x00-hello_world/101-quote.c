b5132455426819187f715b880f3b016cb48188cc
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-18 02:37:10 -0700

    All fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main -Entry point
  *Description: 'Prints a quote'
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 54);
 	return (1);
 }
