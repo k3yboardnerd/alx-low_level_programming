f555c1987380daa6337dbe6a13217c63a0431e9d
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-17 22:02:36 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Description: 'Prints a quote'
+ *Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19",stdout);
+	return (1);
+}
