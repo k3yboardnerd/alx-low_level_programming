0033fc6cc0be2c3182b5664ac9e4e8c00ba348ae
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-18 03:10:49 -0700

    Prints aphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Prints every alphabet in lower case'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
