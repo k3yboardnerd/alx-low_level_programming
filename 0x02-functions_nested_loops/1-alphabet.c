7ea6f6ea08d28542d06d5285b408a3ecbe5e0c06
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-22 15:11:52 -0700

    Fixed the putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include <main.h>
+/**
+ *main - Entry point
+ *Description: 'Prints all alphabets'
+ *Return: Always (Success)
+ */
+int main(void)
+{
+	print_aplhabet()
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
 void pint_alphabet(void);
+
+void print_alphabelt(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
