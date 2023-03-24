7f82010978d83cf61d2689987f28d19faf966a9f
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-24 13:05:01 -0700

    A feww changes

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,15 +10,15 @@ void positive_or_negative(int i)
 {
 	if (i > 0)
 	{
-		printf("%d is postive", i);
+		printf("%d is postive\n", i);
 	}
 	else if (i < 0)
 	{
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
 	}
 	else if (i == 0)
 	{
-		printf("%d is zero", i);
+		printf("%d is zero\n", i);
 	}
 	return (0);
 }
