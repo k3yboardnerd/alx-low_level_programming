96bb76690a7173803ff1a02e1e4e3164268d1bd0
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-24 17:51:35 -0700

    Adding these few codes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two numbers
+ *@a: first integer
+ *@b: second integer
+ *Description: Multiplies 2 numbers
+ *Return: the final value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers (0-9)
+ *Description: This function prints numbers from (0-9)
+ *Return: void
+ */
+void print_numbers(void)
+{
+	char i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+}
