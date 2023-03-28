7c8fdecd47782c4ff4662030d3578d3cada71a05
k3yboardnerd <k3yboardnerd@gmail.com>
2023-03-28 13:03:26 -0700

    Let's start using the pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * reset_to_98 - This function reset the value its points to 98
+ * @n: Pointer to be reset
+ * Description: This function rests the value to a pointer to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
