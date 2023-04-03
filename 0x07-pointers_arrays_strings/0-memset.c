26be9c0f8e0eca24a957d0009da7acdec08352bc
k3yboardnerd <k3yboardnerd@gmail.com>
2023-04-03 08:51:41 -0700

    memset method

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - Fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes
+ * Description: Fills memory with a constant byte
+ * Returns: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *pointer = s;
+
+	while (n-- > 0)
+	{
+		*pointer = b;
+	}
+	return pointer;
+}
