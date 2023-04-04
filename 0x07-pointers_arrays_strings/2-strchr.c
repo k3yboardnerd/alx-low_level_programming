55e37aea7ba131e853dd6786cb03a721a4d4e1d8
k3yboardnerd <k3yboardnerd@gmail.com>
2023-04-03 19:50:32 -0700

    Fixing the strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,15 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0' && *s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
 		s++;
 	}
-	return (NULL);
+
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
