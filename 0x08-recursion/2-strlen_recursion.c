97646fb6e29faab97556e4ccf2ebb15fe2e3dd54
k3yboardnerd <k3yboardnerd@gmail.com>
2023-04-05 12:58:05 -0700

    Lets grt the length of the string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - prints/returns the length of the string
+ * @s: string to be counted
+ * Description: returns the strig length
+ * Return: int/length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\o')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + strlen(s + 1));
+	}
+}
