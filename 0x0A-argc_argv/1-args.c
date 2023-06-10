72319fccc975f87c7e8a1cd25936a35eef108e9e
k3yboardnerd <k3yboardnerd@gmail.com>
2023-06-10 12:59:31 -0700

    Fixed the betty warning'

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 /**
  * main - This program prints the number of args passed into it
  * @argc: the total number of command line arguments
- * @argv: the array of command line arguments values (unused and marked as void)
+ * @argv: the array of command line args values (unused and marked as void)
  * Description: This program prints the number of arguments passed into it
  * Return: 0 if successful
  */
