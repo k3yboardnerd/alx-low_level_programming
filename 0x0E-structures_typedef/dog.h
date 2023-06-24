6628e75b29cf56d6d844b09d04f54fb7037bb5c0
k3yboardnerd <k3yboardnerd@gmail.com>
2023-06-23 17:18:14 -0700

    Forgot to add the description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - This is the dogs structure
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure resembles the details of the dog
+ */
 struct dog
 {
 	char *name;
