42ac1ca4a4613241e281308f936e5c1ee9588230
k3yboardnerd <k3yboardnerd@gmail.com>
2023-06-25 04:55:58 -0700

    I forgot to add the stdlib header, I have also included the else case that will return if either params is NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - This func prints the name from the pointer func
  * @name: the name to be printed
@@ -12,5 +13,10 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL && f != NULL)
 	{
 		f(name);
+	}
+
+	else
+	{
+		return;
 	}
 }
