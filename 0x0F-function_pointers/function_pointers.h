cbb5e63a05110a1b8edb074556db12f092e63707
k3yboardnerd <k3yboardnerd@gmail.com>
2023-06-25 05:59:38 -0700

    This func searches for an int and returns it index matched by the cmp func

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #ifndef FUNC_PTRS
 #define FUNC_PTRS
 
+int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
