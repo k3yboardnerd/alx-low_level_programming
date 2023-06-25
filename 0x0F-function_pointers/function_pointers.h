69fc0cbb172d1cbd0965e3a58e4c3dd61f0b210a
k3yboardnerd <k3yboardnerd@gmail.com>
2023-06-25 05:19:24 -0700

    This function exec a func from func pointer to each elem of the given array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
+#include <stddef.h>
 #ifndef FUNC_PTRS
 #define FUNC_PTRS
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif
