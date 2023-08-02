b107f89b06028231d860b7b942a21529ee339db3
k3yboardnerd <k3yboardnerd@gmail.com>
2023-08-02 12:25:43 -0700

    checks for palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string>
+/**
+ * is_palindrome - checks if the string is palindrome
+ * @s: string to be checked
+ * Description: This func prints 1 if str is palindrome else 0
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int strLen = strlen(s);
+	int i = 0, j = strLen - 1;
+
+	while (i < j)
+	{
+		if (s[i] != s[j])
+		{
+			return (0);
+		}
+		i++;
+		j++;
+	}
+
+	return (1);
+}
