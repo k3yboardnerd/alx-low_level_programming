6939beb795e0659f63733e3ae097d2f8cc2c0a67
k3yboardnerd <k3yboardnerd@gmail.com>
2023-08-02 11:57:55 -0700

    the square of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the square root of a number
+ * @n: nummber to be squared
+ * Description: Returns the square root of a number
+ * Return: square root of n / -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int j = _sqrt_recursion(n / 4) * 2;
+		int m = n - j * j;
+
+		if (m == 0)
+		{
+			return (j);
+		}
+		else if (m > 0)
+		{
+			return (_sqrt_recursion((j + 1) * (j + 1) > n ? n : (j + 1) * (j + 1)));
+		}
+		else
+		{
+			return (_sqrt_recursion((j - 1) * (j - 1) > n ? n : (j - 1) * (j - 1)));
+		}
+	}
+}
