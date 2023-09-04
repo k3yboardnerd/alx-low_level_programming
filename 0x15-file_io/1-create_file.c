f47a9961a28e7d39c728237f325076705e35e898
k3yboardnerd <k3yboardnerd@gmail.com>
2023-09-04 08:18:39 -0700

    Task 1 fxing the error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, S_IRUSER | S_IWUSR);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file == -1)
 		return (-1);
