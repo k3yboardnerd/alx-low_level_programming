a387a50665d681d6b617a35a863bc7e090af0b46
k3yboardnerd <k3yboardnerd@gmail.com>
2023-09-04 06:52:29 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * read_textfile - reads text file and prints it to the POSIX STD
+ * @filename: the filename to be read
+ * @letters: size of letters to be read and printed
+ * Description: reads text file and output it to POSIX STD
+ * Return: 0 if fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d;
+	char *letter_size;
+	ssize_t reader;
+	ssize_t writer;
+
+	if (filename == NULL)
+		return (0);
+
+	file_d = open(filename, 0_RDONLY);
+
+	if (file_d == -1)
+		return (0);
+
+	letter_size = malloc(sizeof(char) * letters);
+
+	if (letter_size == NULL)
+		close(file_d);
+		return (0);
+
+	reader = read(file_d, letter_size, letters);
+
+	if (reader == -1)
+		(free(lettter_size), close(file_d));
+		return (0);
+
+	writer = write(STDOUT_FILENO, letter_size, reader);
+
+	if (writer == -1 || writer != reader)
+		(free(letter_size), close(file_d));
+		return (0);
+
+	free(letter_size);
+	close(file_d);
+	return (writer);
+}
