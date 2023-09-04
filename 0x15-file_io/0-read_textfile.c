c1304dcce3c48cfdd80b4dc8468e200c68f7f005
k3yboardnerd <k3yboardnerd@gmail.com>
2023-09-04 07:45:32 -0700

    Task 0 Fixing the error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 /**
  * read_textfile - reads text file and prints it to the POSIX STD
  * @filename: the filename to be read
@@ -19,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	file_d = open(filename, 0_RDONLY);
+	file_d = open(filename, O_RDONLY);
 
 	if (file_d == -1)
 		return (0);
@@ -27,20 +24,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	letter_size = malloc(sizeof(char) * letters);
 
 	if (letter_size == NULL)
+	{
 		close(file_d);
 		return (0);
+	}
 
 	reader = read(file_d, letter_size, letters);
 
 	if (reader == -1)
-		(free(lettter_size), close(file_d));
+	{
+		(free(letter_size), close(file_d));
 		return (0);
+	}
 
 	writer = write(STDOUT_FILENO, letter_size, reader);
 
 	if (writer == -1 || writer != reader)
+	{
 		(free(letter_size), close(file_d));
 		return (0);
+	}
 
 	free(letter_size);
 	close(file_d);
