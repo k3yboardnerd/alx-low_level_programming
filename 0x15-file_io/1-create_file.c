3ddb703e0d7feba97422edc0517a9b631315f2f8
k3yboardnerd <k3yboardnerd@gmail.com>
2023-09-04 08:12:33 -0700

    Task 1 create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to be created
+ * @text_content: the text/output to be written on the file
+ * Description: creates the file
+ * Return: 1 on success | -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t txt_len = 0;
+	ssize_t writer;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, S_IRUSER | S_IWUSR);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[txt_len] != '\0')
+			txt_len++;
+
+		writer = write(file, text_content, txt_len);
+
+		if (writer == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (-1);
+}
